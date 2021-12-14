b0712a79e601a1863e0583e7834eae32f3c4383e
PulkitBxtra <95132680+PulkitBxtra@users.noreply.github.com>
2021-12-14 22:21:57 +0530

    Create Remove Duplicates from a string by recursion.cpp

---
+++ Day 14/Remove Duplicates from a string by recursion.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+string remove(string s){
+
+    if(s.length()==0){
+        return "";
+    }
+
+    char ch=s[0];
+
+    string ans=remove(s.substr(1));
+
+    if(ch==ans[0]){
+        return ans;
+    }
+    return ch+ans;
+}
+
+int main(){
+    string s="aaaabbbbccccddddeeefff";
+    cout<<remove(s)<<endl;
+
+}
