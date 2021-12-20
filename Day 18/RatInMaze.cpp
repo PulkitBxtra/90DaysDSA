4c452420418471b8ee4877ddac626422d47a98be
PulkitBxtra <95132680+PulkitBxtra@users.noreply.github.com>
2021-12-20 16:56:06 +0530

    Create RatInMaze.cpp

---
+++ Day 18/RatInMaze.cpp
@@ -0,0 +1,74 @@
+ #include<iostream>
+using namespace std;
+
+bool isSafe(int** arr,int x,int y,int n){
+    if(x<n && y<n && arr[x][y]==1){
+        return true;
+    }
+
+    return false;
+}
+
+bool inMaze(int **arr,int x,int y,int n,int** solarr){
+
+    if(x==n-1 && y==n-1){
+        solarr[x][y]=1; 
+        return true;
+    }
+
+    if(isSafe(arr,x,y,n)==true){
+        solarr[x][y]=1;
+        
+        if(inMaze(arr,x+1,y,n,solarr)){
+            return true;
+        }
+
+        if(inMaze(arr,x,y+1,n,solarr)){
+            return true;
+        }
+
+        solarr[x][y]=0;
+        return false;
+    }
+
+    return false;
+}
+
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int **arr=new int*[n];
+    for(int i=0;i<n;i++){
+        arr[i]=new int[n];
+    }
+
+    for(int i=0;i<n;i++){
+       for(int j=0;j<n;j++){
+           cin>>arr[i][j];
+       }
+    }
+
+    int **solarr=new int*[n];
+    
+    for(int i=0;i<n;i++){
+        solarr[i]=new int[n];
+        for(int j=0;j<n;j++){
+            solarr[i][j]=0;
+        }
+    }
+
+    cout<<"answer"<<endl;
+
+    if(inMaze(arr,0,0,n,solarr)){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                cout<<solarr[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+
+}
