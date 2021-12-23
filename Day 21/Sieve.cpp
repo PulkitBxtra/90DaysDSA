35207ff966760549b844624fdb5b1b3b6e3b5b49
PulkitBxtra <95132680+PulkitBxtra@users.noreply.github.com>
2021-12-23 23:23:13 +0530

    Create Sieve.cpp

---
+++ Day 21/Sieve.cpp
@@ -0,0 +1,11 @@
+int countOccurences(int arr[], int n, int X)
+{
+	int counter=0;
+    
+    for(int i=0;i<n;i++){
+        if(arr[i]==X){
+            counter++;
+        }
+    }
+    return counter;
+}
