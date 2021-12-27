9b05794c29a13ba9c7ff7c1e8b915b5e6f3ca364
PulkitBxtra <95132680+PulkitBxtra@users.noreply.github.com>
2021-12-27 22:47:09 +0530

    Create Deletion in doubly linked list.cpp

---
+++ Day 25/Deletion in doubly linked list.cpp
@@ -0,0 +1,124 @@
+#include<iostream>
+using namespace std;
+
+class node{
+
+    public:
+    int data;
+    node* next;
+    node* prev;
+
+    node(int val){
+        data=val;
+        next=NULL;
+        prev=NULL;
+    }
+};
+
+void Insertathead(node* &head,int val){
+    node* n=new node(val);
+    n->next=head;
+
+    if(head!=NULL){
+        head->prev=n;
+    }
+
+    head=n;
+}
+
+void InsertTail(node* &head,int val){
+
+    if(head==NULL){
+        Insertathead(head,val);
+        return;
+    }
+
+    node* n=new node(val);
+    node* temp=head;
+
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+
+    temp->next=n;
+    n->prev=temp;
+
+}
+
+void print(node* &head){
+
+    node* temp=head;
+
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+
+}
+
+node* last(node* &head){
+    node* emp=head;
+    while(emp->next!=NULL){
+        emp=emp->next;
+    }
+    return emp;
+}
+
+void printRev(node* &head){
+
+    node* temp=last(head);
+
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->prev;
+    }  
+    cout<<endl;  
+
+}
+
+void delHead(node* &head){
+    node* todelete=head;
+    head=head->next;
+    head->prev=NULL;
+}
+
+void del(node* &head,int pos){
+    
+    if(pos==1){
+        delHead(head);
+        return;
+
+    }
+    
+    node* temp=head;
+    int count=1;
+    while(temp!=NULL && count<pos){
+        temp=temp->next;
+        count++;
+    }
+    temp->next->prev=temp->prev;
+    temp->prev->next=temp->next;
+    delete temp;
+}
+
+int main(){
+
+    node* head=NULL;
+    InsertTail(head,1);
+    InsertTail(head,2);
+    InsertTail(head,3);
+    InsertTail(head,4);
+    InsertTail(head,5);
+    InsertTail(head,6);
+    print(head);
+    printRev(head);
+    del(head,1);
+    print(head);
+
+
+
+
+
+ return 0;    
+}
