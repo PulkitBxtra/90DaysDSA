27dbc1eb681671d48418266dbce958a4f72e89b7
PulkitBxtra <95132680+PulkitBxtra@users.noreply.github.com>
2022-02-07 20:46:38 +0530

    Create Diameter Of a Tree (More Efficient).cpp

---
+++ Day 42/Diameter Of a Tree (More Efficient).cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int calcDiameter(Node* root,int* height){
+   
+
+    if(root==NULL){
+       *height=0;
+       return 0;
+    }
+
+    int lh=0;
+    int rh=0;
+    int lDiameter=calcDiameter(root->left,&lh);
+    int rDiameter=calcDiameter(root->right,&rh);
+
+    int currDiameter=lh+rh+1;
+    *height=max(lh,rh)+1;
+
+    return max(currDiameter,max(lDiameter,rDiameter));
+
+
+}
+
+int main(){
+
+    Node* root=new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(3);
+    root->left->left=new Node(4);
+    root->left->right=new Node(5);
+    root->right->right=new Node(6);
+    root->right->left=new Node(7);
+
+    int height=0;
+
+    cout<<calcDiameter(root,&height)<<endl;
+}
