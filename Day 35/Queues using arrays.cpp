dcc6557ad81548e1d1b6b7d0e646a165aa5b4bf0
PulkitBxtra <95132680+PulkitBxtra@users.noreply.github.com>
2022-08-09 22:36:31 +0530

    Update Queues using arrays.cpp

--- Day 35/Queues using arrays.cpp
+++ Day 35/Queues using arrays.cpp
@@ -1,78 +1,61 @@
 #include<iostream>
 using namespace std;
 
-#define n 20
-
 class queue{
-    int* arr;
-    int front;
-    int back;
-    public:
-
-    queue(){
-        arr=new int[n];
-        front=-1;
-        back=-1;
-    }
-
-    void push(int x){
-        if(back==n-1){
-            cout<<"queue overload"<<endl;
-        }
-
-        back++;
-        arr[back]=x;
     
-        if(front==-1){
-            front++;
-        }
-    }
+    private:
+    int arr[10];
+    int front=-1;
+    int back;
 
-    void pop(){
-        if(front==-1 || front>back){
-            return;
-        }
-        front++;
-    }
-
-    int peek(){
-        if(front==-1||front>back){
-            return -1;
-        }
-
-        else{
-            return arr[front];
-        }
-    }
-
+    public:
     bool empty(){
-        if(front==-1|| front>back){
+
+        if(front==-1){
             return true;
         }
         return false;
     }
-};
 
+    int peek(){
+        if(front==-1){
+            return -1;
+        }
+
+        return arr[front];
+    }
+
+    bool full(){
+        if(front==10){
+            return true;
+        }
+
+        return false;
+    }
+
+
+
+    void enque(int val){
+        front++;
+        arr[front]=val;
+    }
+
+    void deque(){
+        front--;
+    }
+};
 
 int main(){
 
-    queue q;
-    q.push(1); 
-    q.push(2);
-    q.push(3);
-    q.push(4);
+    queue qu;
 
-    cout<<q.peek()<<endl;
+    qu.enque(1);   
+    qu.enque(2);   
+    qu.enque(3);   
+    qu.enque(4);   
+    // qu.enque(4);
 
-    q.pop();
+    qu.deque();
+    cout<<qu.peek()<<endl; 
 
-    cout<<q.peek()<<endl;
-
-    q.pop();
-    q.pop();
-    q.peek();
-
-    cout<<q.empty()<<endl;;
-
- return 0;
 }
